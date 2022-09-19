e5fb636ca219f7cee1dd9d512da66cb45de637f8
sk924 <0924wisdom@naver.com>
2022-09-19 13:55:50 +0900

    pug task-3(2를 정리함)

--- gulpfile.babel copy.js
+++ gulpfile.babel copy.js
@@ -1,3 +1,29 @@
+// import gulp from "gulp";
+// import gpug from "gulp-pug";
+// import del from "del";
+
+// const routes = {
+//   pug: {
+//     src: "src/*.pug",
+//     dest: "build"
+//   },
+// };
+
+// //3)
+// const pug = () =>
+//   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+// //1)
+// const clean = () => del(["build"]);
+
+// //4)
+// const prepare = () => gulp.series([clean]);
+
+// //4)
+// const assets = () => gulp.series([pug]);
+
+// //2) //5)
+// export const dev = gulp.series([prepare, assets]);
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
@@ -6,25 +32,22 @@ const routes = {
   pug: {
     src: "src/*.pug",
     dest: "build"
-  },
+  }
 };
 
-//3)
 const pug = () =>
-  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+  gulp
+    .src(routes.pug.src)
+    .pipe(gpug())
+    .pipe(gulp.dest(routes.pug.dest));
 
-//1)
 const clean = () => del(["build"]);
 
-//4)
 const prepare = gulp.series([clean]);
 
-//4)
 const assets = gulp.series([pug]);
 
-//2) //5)
 export const dev = gulp.series([prepare, assets]);
-
 /*
 1) clean이라는 task를 만들고, 얘가 할 일을 작성해줌. 즉 build폴더를 삭제해줌
 2) clean이라는 task를 series에 추가해줌
